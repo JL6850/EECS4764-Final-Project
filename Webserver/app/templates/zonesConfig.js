67965a6d4cf16479046f879e005efcc4298ac851
JL6850 <jl6850@columbia.edu>
2024-11-26 17:13:07 -0500

    jl6850 Update the logic of card html

--- Webserver/app/templates/zonesConfig.js
+++
@@ -1,7 +0,0 @@
-const zonesConfig = [
-    { type: 'extra-monster', label: '额外怪兽区' },
-    { type: 'field', label: '场地魔法区' },
-    { type: 'monster', label: '怪兽区' },
-    { type: 'empty', label: '空区域' },
-    { type: 'graveyard', label: '墓地' }, // 新增的区域
-];
